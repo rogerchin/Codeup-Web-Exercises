049167bfc4f36e7b435ca0b06c6d14db8e4e338b
ROGER CHIN <Roger@ROGERs-MacBook-Pro.local>
2015-10-06 16:00:25 -0500

    New JS practices

---
+++ public/js/external.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("We fight for the users!")
+
+if (raining) {
+	console.log('Bring an umbrella ella ella ay')
+} else if (foggy) {
+
+} else if (sunny) {
+
+} else {
+
+function isEven(input) {
+	return (input % 2 === 0) ? true : false;
+}
+
+// for(var i = 0; i <= 100; i++) {
+// 	if(isEven(i)) {
+// 		console.log(i+' is even')
+// 	}
+// }
+
+console.log(isEven(i) ? i + 'is even' + 'is odd');
