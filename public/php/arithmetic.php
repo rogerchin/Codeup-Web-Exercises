c589b0359ce39476bb9ea15b2bd0272a95655ea2
ROGER CHIN <Roger@ROGERs-MacBook-Pro.local>
2015-11-05 15:54:49 -0600

    Adding more PHP exercises

---
+++ public/php/arithmetic.php
@@ -0,0 +1,36 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+
+
+echo add(8,5) . PHP_EOL;
+echo subtract(8,5) . PHP_EOL;
+echo multiply(8,5) . PHP_EOL;
+echo divide(8,5) . PHP_EOL;
+echo modulus(8,5) . PHP_EOL;
+
+// Add code to test your functions here
+
